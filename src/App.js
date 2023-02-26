841a9faed8def8cebe0244209c1f0f6e66f6e1d2
PotapenkoVadim <potapenko.v00@mail.ru>
2023-02-26 12:32:18 +0300

    build: init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import { ScrollSmoother } from 'gsap/ScrollSmoother'
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
